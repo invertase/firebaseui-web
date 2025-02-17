9a0da11647723bdc81a3c239d848d39377a64022
Darren Ackers <ackers86@hotmail.com>
2025-02-17 18:42:21 +0000

    chore: renamed app to angular

--- packages/angular/src/app/app.component.ts
+++ packages/angular/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonComponent} from "../../firebaseui-angular/dist/";
+import {ButtonComponent} from "../../firebaseui-angular/dist";
 
 @Component({
   selector: 'app-root',
