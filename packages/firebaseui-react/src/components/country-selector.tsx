3e5ba150608d9dcc589014ab70f2fd478bd3e355
Elliot Hesp <elliot.hesp@gmail.com>
2025-02-19 13:13:41 +0000

    fix: Remove country name from country picker

--- packages/firebaseui-react/src/components/country-selector.tsx
+++ packages/firebaseui-react/src/components/country-selector.tsx
@@ -18,24 +18,28 @@ export function CountrySelector({
     <div className={cn("fui-country-selector", className)}>
       <div className="fui-country-selector__wrapper">
         <span className="fui-country-selector__flag">{value.emoji}</span>
-        <select
-          value={value.code}
-          onChange={(e) => {
-            const country = countryData.find((c) => c.code === e.target.value);
-            if (country) {
-              onChange(country);
-            }
-          }}
-        >
-          {countryData.map((country) => (
-            <option
-              key={`${country.code}-${country.dialCode}`}
-              value={country.code}
-            >
-              {country.dialCode} ({country.name})
-            </option>
-          ))}
-        </select>
+        <div className="fui-country-selector__select-wrapper">
+          <span className="fui-country-selector__dial-code">{value.dialCode}</span>
+          <select
+            className="fui-country-selector__select"
+            value={value.code}
+            onChange={(e) => {
+              const country = countryData.find((c) => c.code === e.target.value);
+              if (country) {
+                onChange(country);
+              }
+            }}
+          >
+            {countryData.map((country) => (
+              <option
+                key={`${country.code}-${country.dialCode}`}
+                value={country.code}
+              >
+                {country.dialCode} ({country.name})
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </div>
   );
