f47fd386e990d20f03a731aabe10ba3342b6aad1
Darren Ackers <ackers86@hotmail.com>
2025-04-17 12:36:15 +0100

    fix(nextjs): removed obsolete classname on examples

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="mb-6">
         {user && <div>Welcome: {user.email || user.phoneNumber}</div>}
       </div>
-      <div className="">
+      <div>
         <h2 className="text-2xl font-bold mb-4">Auth Screens</h2>
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           <li>
